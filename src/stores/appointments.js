dc0b531a9547c692865d178ec766bdaca39e1709
Jair <jairaceves56@gmail.com>
2023-12-20 15:50:10 -0600

    Create the Select or Not select Appoinment Function

---
+++ src/stores/appointments.js
@@ -0,0 +1,35 @@
+import { ref, computed, onMounted, inject, watch } from 'vue'
+import { defineStore } from 'pinia'
+import { useRouter } from 'vue-router'
+
+
+export const useAppointmentsStore = defineStore('appointments', () => {
+
+    const services = ref([])
+
+    function onServiceSelected(service) {
+        if(services.value.some(selectedService => selectedService._id === service._id)) {
+            services.value = services.value.filter( selectedService => selectedService._id !== service._id )
+        } else {
+            // if(services.value.length === 2) {
+            //     alert('Maximum 2 services per appointment')
+            //     return
+            // }
+            services.value.push(service)
+        }
+    }
+
+    const isServiceSelected = computed(() => {
+        return (id) =>  services.value.some( service => service._id === id )
+    })
+
+    const noServicesSelected = computed(() => services.value.length === 0) 
+
+
+    return {
+        services,
+        onServiceSelected,
+        isServiceSelected,
+        noServicesSelected,
+    }
+})
