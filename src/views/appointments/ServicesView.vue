23938939659464768d5f90a85a473ab6058fc2d7
Jair <jairaceves56@gmail.com>
2023-12-20 14:23:06 -0600

    Create the Services View

--- src/views/appointments/ServicesView.vue
+++ src/views/appointments/ServicesView.vue
@@ -1,9 +1,23 @@
 <script setup>
-
+    import ServiceItem from '../../components/ServiceItem.vue'
+    import { useServicesStore } from '@/stores/services';
+    
+    
+    const store  = useServicesStore()
 </script>
 
 <template>
-    <h2 class="text-4xl font-extrabold text-white">Desde Servicios</h2>
+    <div>
+        <h2 class="text-4xl font-extrabold text-white mt-10">Services</h2>
+        <p class="text-white text-lg mt-5">Choose at least one service for your appointment</p>
     
+        <div class="grid md:grid-cols-2 lg:md:grid-cols-3 gap-5 mt-5">
+            <ServiceItem
+                v-for="service in store.services"
+                :key="service._id"
+                :service="service"
+            />
+        </div>
+    </div>
 </template>
 
