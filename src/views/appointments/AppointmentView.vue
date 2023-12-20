be11946e6e38e4beb91271d20b7a0fd9a0eef621
Jair <jairaceves56@gmail.com>
2023-12-20 16:17:49 -0600

    Create the View to show the services selected

--- src/views/appointments/AppointmentView.vue
+++ src/views/appointments/AppointmentView.vue
@@ -1,9 +1,24 @@
 <script setup>
-   
+    import SelectedService from '@/components/SelectedService.vue';
+    import { useAppointmentsStore } from '../../stores/appointments';
+
+    const appointments = useAppointmentsStore()
 </script>
 
 
 <template>
-    <h2 class="text-4xl font-extrabold text-white">Detalles Cita y Resumen</h2>
-    
+    <div>
+        <h2 class="text-4xl font-extrabold text-white">Detalles Cita y Resumen</h2>
+        <p class="text-white text-lg mt-5">Confirm the Information and Confirm your Appointment</p>
+
+        <h3 class="text-3xl font-extrabold text-white">Services</h3>
+
+        <div class="grid gap-5 mt-5">
+            <SelectedService 
+                v-for="service in appointments.services"
+                :key="service._id"
+                :service="service"
+            />
+        </div>
+    </div>
 </template>
