7e75690b72d6d0be57042a2d391d2dc9b27c4329
Jair <jairaceves56@gmail.com>
2023-12-20 16:17:15 -0600

    Show all the Service that the user selected

---
+++ src/components/SelectedService.vue
@@ -0,0 +1,30 @@
+<script setup>
+    import { useAppointmentsStore } from '../stores/appointments';
+    import { formatCurrency } from '../helpers';
+
+    const appointments = useAppointmentsStore()
+    defineProps({
+        service: {
+            type: Object
+        }
+    })
+</script>
+
+
+<template>
+    <div class="border-2 border-gray-700 shadow-xl rounded-lg p-5 flex justify-between items-center">
+        <div>
+            <p class="text-2xl font-extrabold text-white">{{ service.name }}</p>
+            <p class="text-3xl font-black text-blue-500">{{ formatCurrency( service.price ) }}</p>
+        </div>
+        
+        <button
+            type="button"
+            class="inline-flex items-center rounded-md bg-red-600 duration-300 text-white text-sm px-3 py-2 shadow-sm uppercase font-bold hover:bg-red-700"
+            @click="appointments.onServiceSelected(service)"
+        >
+            Delete
+        </button>
+    </div>
+</template>
+
