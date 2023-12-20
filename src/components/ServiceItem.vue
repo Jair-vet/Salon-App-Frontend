9ee24e8d066151e99baddf8aca7e227ff3085dc6
Jair <jairaceves56@gmail.com>
2023-12-20 15:30:46 -0600

    create each Service Item Component

--- src/components/ServiceItem.vue
+++ src/components/ServiceItem.vue
@@ -1,8 +1,8 @@
 <script setup>
-    // import { formatCurrency } from '../helpers'
-    // import { useAppointmentsStore } from '../stores/appointments'
+    import { formatCurrency } from '../helpers'
+    import { useAppointmentsStore } from '../stores/appointments'
 
-    // const appointments = useAppointmentsStore()
+    const appointments = useAppointmentsStore()
 
     defineProps({
         service: {
@@ -14,14 +14,14 @@
 
 <template>
     <div
-        class="p-5 space-y-5 rounded-lg cursor-pointer"
-        :class="appointments.isServiceSelected(service._id) ? 'bg-blue-500 text-white' : 'bg-white'"
+        class="p-5 space-y-5 rounded-lg cursor-pointer duration-500 md:h-32 hover:bg-gray-500 hover:text-white"
+        :class="appointments.isServiceSelected(service._id) ? 'bg-blue-500 text-white' : 'bg-gray-200'"
         @click="appointments.onServiceSelected(service)"
     >
-        <p class="text-2xl font-light">{{ service.name }}</p>
+        <p class="text-xl font-light">{{ service.name }}</p>
         <p 
-            class="text-4xl font-black"
-            :class="appointments.isServiceSelected(service._id) ? 'text-white' : 'text-blue-600'"
-        >{{ service.price  }}</p>
+            class="text-2xl font-black"
+            :class="appointments.isServiceSelected(service._id) ? 'text-white' : 'text-green-500'"
+        >{{ formatCurrency(service.price)  }}</p>
     </div>
 </template>
