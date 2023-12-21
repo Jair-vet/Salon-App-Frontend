0f8338ecc5c2649b95ebea7c90eb5f5d5c46a897
Jair <jairaceves56@gmail.com>
2023-12-21 11:39:45 -0600

    Create the Layout to Auth

---
+++ src/views/auth/AuthLayout.vue
@@ -0,0 +1,28 @@
+<script setup>
+    import { useRoute } from 'vue-router'
+    const route = useRoute()
+
+    const authRoutes = [
+        { name: 'register', text: 'Create Account' },
+        { name: 'login', text: 'Log In' },
+        // { name: 'forgot-password', text: 'Forgot Password' },
+    ]
+</script>
+
+<template>
+    <div class="mx-auto lg:w-3/5 mt-20">
+        <RouterView />
+
+        <nav class="mt-10 flex flex-col items-center space-y-5 lg:flex-row lg:justify-between lg:space-y-0">
+            <RouterLink
+                v-for="authRoute in authRoutes"
+                :key="authRoute.id"
+                class="uppercase font-bold text-white md:p-3 p-1 rounded-md border shadow-xl border-gray-800 duration-300 hover:text-blue-500 hover:border hover:border-blue-500"
+                :to="{name: authRoute.name}"
+                :class="{ 'hidden' : route.name === authRoute.name }"
+
+            >   {{ authRoute.text }}
+            </RouterLink>
+        </nav>
+    </div>
+</template>
