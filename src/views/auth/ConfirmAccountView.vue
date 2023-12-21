c294a35f2284630b61eb499e1d0d59ee0e12b96b
Jair <jairaceves56@gmail.com>
2023-12-21 14:02:34 -0600

    Confirm Account done

--- src/views/auth/ConfirmAccountView.vue
+++ src/views/auth/ConfirmAccountView.vue
@@ -1,30 +1,30 @@
 <script setup>
-//     import { onMounted, inject } from 'vue'
-//     import { useRoute, useRouter } from 'vue-router'
-//     import AuthAPI from '../../api/AuthAPI'
-// 
-//     const toast = inject('toast')
-//     const router = useRouter()
-//     const route = useRoute()
-//     const { token } = route.params
-// 
-//     onMounted(async () => {
-//         try {
-//             const { data } = await AuthAPI.verifyAccount(token)
-//             toast.open({
-//                 message: data.msg,
-//                 type: 'success'
-//             })
-//             setTimeout(() => {
-//                 router.push({name: 'login'})
-//             }, 5000);
-//         } catch (error) {
-//             toast.open({
-//                 message: error.response.data.msg,
-//                 type: 'error'
-//             })
-//         }
-//     })
+    import { onMounted, inject } from 'vue'
+    import { useRoute, useRouter } from 'vue-router'
+    import AuthAPI from '../../api/AuthAPI'
+
+    const toast = inject('toast')
+    const router = useRouter()
+    const route = useRoute()
+    const { token } = route.params
+
+    onMounted(async () => {
+        try {
+            const { data } = await AuthAPI.verifyAccount(token)
+            toast.open({
+                message: data.msg,
+                type: 'success'
+            })
+            setTimeout(() => {
+                router.push({name: 'login'})
+            }, 5000);
+        } catch (error) {
+            toast.open({
+                message: error.response.data.msg,
+                type: 'error'
+            })
+        }
+    })
 </script>
 
 <template>
