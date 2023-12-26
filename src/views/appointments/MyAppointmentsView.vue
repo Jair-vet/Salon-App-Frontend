224c1584a61d2df4c7ec239116df2ac81c9f70bc
Jair <jairaceves56@gmail.com>
2023-12-26 13:41:54 -0600

    Show all Dates that user have been done

--- src/views/appointments/MyAppointmentsView.vue
+++ src/views/appointments/MyAppointmentsView.vue
@@ -1,5 +1,5 @@
 <script setup>
-    // import Appointment from '../../components/Appointment.vue'
+    import Appointment from '@/components/Appointment.vue'
     import { useUserStore } from '../../stores/user'
     const user = useUserStore()
 </script>
@@ -11,7 +11,7 @@
     
         <p v-if="user.loading" class="text-white text-2xl text-center mt-5">Loading...</p>
     
-        <div >
+        <div v-else>
             <p v-if="user.noAppointments" class="text-yellow-600 uppercase font-bold text-2xl text-center mt-20">You have no upcoming appointments</p>
     
     
